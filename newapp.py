4e2b5c502347b6e593632f2078d81ed8ab82d876
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2021-05-18 13:45:01 +0300

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,20 +1,54 @@
-
-import torch
 import streamlit as st
-
-model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model='best.pt')
-
 from PIL import Image
+from urllib.request import urlretrieve
+from fastai.vision.widgets import *
+from fastai.vision.all import *
 
-# Images
-img1 = Image.open('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg')
+
+url = ("http://dl.dropboxusercontent.com/s/fkdy4rbf8g8wm2s/best.pt?raw=1")
+filename = "best.pt"
+urlretrieve(url,filename)
+
+urll = ("http://dl.dropboxusercontent.com/s/1mosvbcftjgpm1y/WhatsApp%20Image%202021-03-31%20at%2022.59.16.jpeg?raw=1")
+filenamee = "1.png"
+urlretrieve(urll,filenamee)
+st.image(filenamee)
+st.write('# KAN HÜCRESİ TESPİTİ')
 
 
-results = model(img1, size=320)
+uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
 
-# Inference
-results = model(img1, size=640)  # includes NMS
-results.print()  
-results.save()  # or .show()
-st.image("results/BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg")
-# Data
+
+
+if uploaded_file is None:
+    # Default image.
+    url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
+    image = Image.open(requests.get(url, stream=True).raw)
+
+else:
+    image = Image.open(uploaded_file)
+    img_array = np.array(image)
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=filename)
+    model.results = model(img_array, size=640)
+    model.results.save()  # or .show()
+    st.image("results/image0.jpg")
+  
+    liste = []
+    liste1 = []
+    liste2 = []
+    liste0 = []
+
+
+    for i in model.results.xywh:
+        for j in i:
+            for k in j:
+                liste.append(k)
+            if k ==2:
+                liste2.append(k)
+            elif k == 1:
+                liste1.append(k)
+            elif k == 0:
+                liste0.append(k)
+
+
+    st.write("The number of detected WBC is",len(liste2),"The number of detected RBC is",len(liste1),"The number of detected PLT is",len(liste0))
