8c86f375fd9cbf1d45b14bdd002705bb3fc9c3ee
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2021-09-29 23:03:07 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -39,9 +39,9 @@ else:
     model.conf = 0.75
     #model.conf = st.sidebar.slider("Confidence threshold", 0.0, 1.0, 0.5, 0.01)
     
-    #model.results = model(img_array, size=512)
-    #model.results.save("yolov5/results")
-    #model.results.save()
+    model.results = model(img_array, size=512)
+    model.results.save("yolov5/results")
+    model.results.save()
     ######
    
         
