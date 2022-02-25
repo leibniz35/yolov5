4485ccee35964b330e631f213330b6a80b119536
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2022-02-26 00:35:19 +0300

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -56,7 +56,7 @@ st.write('mAP')
 url3 = ("http://dl.dropboxusercontent.com/s/pnd6p3zbibx6ofa/map.png?raw=1")
 filenameee = "map.png"
 urlretrieve(url3,filenameee)
-st.image(filenameee)
+st.button(st.image(filenameee))
     
 st.write('Losses')
     
@@ -64,7 +64,8 @@ st.write('Losses')
 url4 = ("http://dl.dropboxusercontent.com/s/0sveaoty719d2ie/losses.png?raw=1")
 filenameeee = "losses.png"
 urlretrieve(url4,filenameeee)
-st.image(filenameeee)
+st.button(st.image(filenameeee))
+
     
     
     
