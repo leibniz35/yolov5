1979e74d11cbf398f90c2de94b141e4bdd29d89f
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2022-03-02 22:00:38 +0300

    Create colonyapp-v1.py

---
+++ colonyapp-v1.py
@@ -0,0 +1,97 @@
+__author__ = "Mehmet Değirmenci"
+
+
+import streamlit as st
+from PIL import Image
+from urllib.request import urlretrieve
+from fastai.vision.widgets import *
+from fastai.vision.all import *
+
+
+
+
+url = ("http://dl.dropboxusercontent.com/s/puam7atr8213pks/colony.pt?raw=1")
+filename = "colony.pt"
+urlretrieve(url,filename)
+
+
+
+urll = ("http://dl.dropboxusercontent.com/s/ecl4tj6q2u8s4q3/fig-03_5.png?raw=1")
+filenamee = "1.png"
+urlretrieve(urll,filenamee)
+st.image(filenamee)
+st.write('# COLONY COUNTER V.1')
+
+
+
+
+uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+
+
+
+if uploaded_file is None:
+    # Default image.
+    url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
+    image = Image.open(requests.get(url, stream=True).raw)
+
+else:
+    image = Image.open(uploaded_file)
+    img_array = np.array(image)
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=filename)
+    model.conf = 0.40
+    model.conf = st.sidebar.slider("Confidence threshold", 0.0, 1.0, 0.5, 0.01)
+    
+    model.results = model(img_array, size=512)
+    model.results.save("yolov5/results")
+    model.results.save()
+    st.image("yolov5/results/image0.jpg")
+    ######
+   
+        
+        
+  
+        
+
+
+        
+
+        
+  
+
+ #######
+       
+ 
+    liste = []
+    liste1 = []
+    liste2 = []
+    liste0 = []
+    liste3 = []
+    liste4 = []
+    liste5 = []
+    liste6 = []
+
+
+    for i in model.results.xywh:
+        for j in i:
+            for k in j:
+                liste.append(k)
+            if k ==2:
+                liste2.append(k)
+            elif k == 1:
+                liste1.append(k)
+            elif k == 3:
+                liste3.append(k)
+            elif k == 4:
+                liste4.append(k)
+            elif k == 5:
+                liste5.append(k) 
+            elif k == 6:
+                liste6.append(k)
+            elif k == 0:
+                liste0.append(k)
+            
+
+    st.write("Number of colony detected:",len(liste0) +len(liste1) +len(liste2) +len(liste3) +len(liste4) +len(liste5) +len(liste6))
+   
+    
+    
