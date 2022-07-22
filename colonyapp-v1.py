a5559bbd2d995c1c1caa788249e05c11451b96c4
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2022-07-23 00:27:06 +0300

    Update colonyapp-v1.py

--- colonyapp-v1.py
+++ colonyapp-v1.py
@@ -44,7 +44,7 @@ else:
     model.results = model(img_array, size=512)
     #model.results.save("yolov5/results")
     model.results.save()
-    st.image("yolov5/runs/detect/exp/4.png")
+    st.image("yolov5/runs/detect/exp/iamge0.jpg")
     ######
    
         
