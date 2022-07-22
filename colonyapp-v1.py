67e2a4488045db577bad0780862cc24c05cc8c78
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2022-07-23 00:13:33 +0300

    Update colonyapp-v1.py

--- colonyapp-v1.py
+++ colonyapp-v1.py
@@ -42,8 +42,8 @@ else:
     model.conf = st.sidebar.slider("Confidence threshold", 0.0, 1.0, 0.5, 0.01)
     
     model.results = model(img_array, size=512)
-    #model.results.save("yolov5/results")
-    model.results.save()
+    model.results.save("yolov5/results")
+    #model.results.save()
     #st.image("yolov5/results/*image0.jpg")
     ######
    
